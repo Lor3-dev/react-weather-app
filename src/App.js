ac6481c3d900874d5b5af1b20765d6e17578d968
Lor <lor.develop@gmail.com>
2021-04-10 17:30:25 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello from React</div>;
+  return (
+    <div className="App">
+      Hello from React{" "}
+      <footer>
+        This project was coded by Lorena Ibeas and is {""}
+        <a href="https://github.com/Lor3-dev/react-weather-app" target="_blank">
+          open-sourced on GItHub
+        </a>
+      </footer>
+    </div>
+  );
 }
