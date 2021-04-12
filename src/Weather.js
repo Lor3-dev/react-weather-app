23419e029f4f1018a7ec54393c91d932d42081e8
Lor <lor.develop@gmail.com>
2021-04-12 08:38:14 +0200

    Updated Icon

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       description: response.data.weather[0].description,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      icon: response.data.weather[0].icon,
       ready: true,
     });
   }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,11 +1,18 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <div className="row">
         <div className="col-6">
-          <img src={props.data.iconUrl} alt={props.data.description} />
+          <WeatherIcon
+            code={props.data.icon}
+            icon="CLEAR_DAY"
+            color="goldenrod"
+            size={40}
+            animate={true}
+          />
           <span className="temperature">
             {Math.round(props.data.temperature)}
           </span>{" "}
